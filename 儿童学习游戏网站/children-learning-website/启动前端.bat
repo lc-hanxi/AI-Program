b5cae6863debb8f81865c909bed1c74e055e95b3
lc <lc19890709@126.com>
2025-04-09 22:23:49 +0800

    lc 删除无用脚本

--- 儿童学习游戏网站/children-learning-website/启动前端.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem 设置为UTF-8编码
-chcp 65001 > nul
-title 前端服务器
-
-cd frontend
-npm run serve
-pause 
--- 儿童学习游戏网站/children-learning-website/启动后端.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem 设置为UTF-8编码
-chcp 65001 > nul
-title 后端服务器
-
-cd backend
-node server.js
-pause 
